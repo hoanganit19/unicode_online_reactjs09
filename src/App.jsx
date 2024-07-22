5c59904bb18005520c77500f5592112c40a672af
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-07-22 16:12:39 +0700

    Redux Core 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
 export default function App() {
-  return <div>App</div>;
+  const count = useSelector((state) => {
+    return state.count;
+  });
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch({ type: "counter/increment" });
+  };
+  const handleDecrement = () => {
+    dispatch({ type: "counter/decrement" });
+  };
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={handleDecrement}>-</button>
+      <button onClick={handleIncrement}>+</button>
+    </div>
+  );
 }
